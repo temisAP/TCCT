723aa20066890adaea2d84bc761a993adcc71117
temisAP <74715759+temisAP@users.noreply.github.com>
2021-04-27 11:10:51 +0200

    gitignore

---
+++ ESATAN/Modelos_ESATAN/ESATAN-TMS_Models/Satelite/esatan/acase01/SATELITE.f
@@ -0,0 +1,145 @@
+      PROGRAM SATELITE_ACASE01                                                  
+C   
+      INCLUDE 'SATELITE.h'                                                      
+C   
+      CHARACTER MNAME * 24                                                      
+C   
+      MNAME = 'SATELITE_ACASE01        '                                        
+C   
+      FLG(1) = 130                                                              
+      FLG(2) = 204                                                              
+      FLG(3) = 0                                                                
+      FLG(4) = 1372                                                             
+      FLG(5) = 0                                                                
+      FLG(6) = 1                                                                
+      FLG(7) = 0                                                                
+      FLG(8) = 0                                                                
+      FLG(9) = 0                                                                
+      FLG(10) = 0                                                               
+      FLG(11) = 2                                                               
+      FLG(12) = 0                                                               
+      FLG(13) = 49                                                              
+      FLG(14) = 25                                                              
+      FLG(15) = 16                                                              
+      FLG(16) = 1                                                               
+      FLG(17) = 92                                                              
+      FLG(18) = 0                                                               
+      FLG(19) = 0                                                               
+      FLG(20) = 0                                                               
+      FLG(21) = 0                                                               
+      FLG(22) = 0                                                               
+      FLG(23) = 0                                                               
+      FLG(24) = 7106                                                            
+      FLG(25) = 2                                                               
+      FLG(26) = 1                                                               
+      FLG(27) = 133                                                             
+      FLG(28) = 1003396                                                         
+      FLG(29) = 1                                                               
+      FLG(30) = 0                                                               
+      FLG(31) = 0                                                               
+      FLG(32) = 0                                                               
+      FLG(33) = 0                                                               
+      FLG(34) = 0                                                               
+      FLG(35) = 22                                                              
+      FLG(36) = 1                                                               
+      FLG(37) = 0                                                               
+      FLG(38) = 0                                                               
+      FLG(39) = 2744                                                            
+      FLG(40) = 0                                                               
+      FLG(41) = 0                                                               
+      CALL SVMNAM(MNAME)                                                        
+C   
+      SPRNDM = 1                                                                
+      SPRNDN = 3                                                                
+      USRNDC = 0                                                                
+      USRNDI = 0                                                                
+      USRNDR = 0                                                                
+C   
+      CALL MAINA(MNAME)                                                         
+C   
+      CALL SUCCES                                                               
+C   
+      STOP                                                                      
+C   
+      END                                                                       
+      SUBROUTINE IN0001                                                 
+      INCLUDE 'SATELITE.h'
+      LOGICAL HTFLAG                                                    
+      HTFLAG = .TRUE.                                                   
+      OPBLOK = 'INITIAL   '                                             
+      IG(2) = 1                                                         
+      CALL GM0001(HTFLAG)                                               
+      CALL FINITS(' ' , -    1)                                         
+      OPBLOK = ' '                                                      
+      RETURN                                                            
+      END                                                               
+      SUBROUTINE V10001                                                 
+      INCLUDE 'SATELITE.h'
+      LOGICAL HTFLAG                                                    
+      HTFLAG = (SOLTYP .EQ. 'THERMAL' .OR. SOLTYP .EQ. 'HUMID')         
+      OPBLOK = 'VARIABLES1'                                             
+      IG(2) = 1                                                         
+      CALL PRMUPD                                                       
+      CALL CHKTRM                                                       
+      CALL ACDDYU                                                       
+      CALL HEATER_UPDATE                                                
+      IG(2) = 1                                                         
+      CALL GM0001(HTFLAG)                                               
+      OPBLOK = ' '                                                      
+      RETURN                                                            
+      END                                                               
+      SUBROUTINE GM0001(HTFLAG)                                         
+      LOGICAL HTFLAG                                                    
+      RETURN                                                            
+      END                                                               
+      SUBROUTINE V20001                                                 
+      INCLUDE 'SATELITE.h'
+      OPBLOK = 'VARIABLES2'                                             
+      IG(2) = 1                                                         
+      CALL SSNCNT(FLG(24),FLG(25),MAX0(FLG(1),1),PCS,T)                 
+      IG(25) = IG(25) + 1                                               
+      CALL PRMUPD                                                       
+      CALL HEATER_UPDATE                                                
+      IG(2) = 1                                                         
+      OPBLOK = ' '                                                      
+      CALL PARWRT('VARIABLES2')                                         
+      RETURN                                                            
+      END                                                               
+      SUBROUTINE EXECTN                                                 
+      INCLUDE 'SATELITE.h'
+      RG(13)=0.01                                                       
+      IG(4)=100                                                         
+      CALL SOLVFM                                                       
+      RG(18)=RU(MD(8,1)+1)                                              
+      RG(3)=RG(18)/100.0                                                
+      RG(12)=RG(18)/10.0                                                
+      CALL SLCRNC                                                       
+      IG(4)=500                                                         
+      RG(13)=0.01                                                       
+      CALL SOLVFM                                                       
+      RG(18)=6050.2473                                                  
+      RG(12)=605.02473                                                  
+      IG(4)=500                                                         
+      RG(13)=0.01                                                       
+      RG(3)=0.01                                                        
+      CALL SOLCYC('SLCRNC',0.01D0,0.01D0,6050.2473D0,10,' ','NONE')     
+      CALL SLCRNC                                                       
+      RG(18)=6050.2473                                                  
+      RG(12)=605.02473                                                  
+      IG(4)=500                                                         
+      RG(13)=0.01                                                       
+      RG(3)=10.0                                                        
+      CALL SOLCYC('SLCRNC',0.01D0,0.01D0,6050.2473D0,10,' ','NONE')     
+      CALL SLCRNC                                                       
+      RETURN                                                            
+      END                                                               
+      SUBROUTINE OUTPUT                                                 
+      INCLUDE 'SATELITE.h'
+      IF (OUTIME .NE. 'ALL') RETURN                                     
+      OPBLOK = 'OUTPUTS'                                                
+      CALL PRNDTB(' ','L, T, QS, QE, QA, QI, C',1)                      
+      CALL DMPTMD(' ','NODES, CONDUCTORS, CONSTANTS',1,' ')             
+      OPBLOK = ' '                                                      
+      CALL PARWRT('OUTPUTS')                                            
+      RETURN                                                            
+      END                                                               
